f8a1f1785132b9937e4f2df6309f2f4e683cd1d8
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-04-03 11:57:45 -0300

    fix: config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,27 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import pg from 'pg';
+import dotenv from 'dotenv';
+import chalk from 'chalk';
 dotenv.config();
 
 const { Pool } = pg;
 
-export const connectionDb = new Pool({
+const configDatabase = {
   connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === "production" && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+};
+
+export const db = new Pool(configDatabase);
+
+db.connect((err) => {
+  if (err) {
+    console.error(chalk.red('Erro de conexão no Database:'), err.stack);
+  } else {
+    console.log(chalk.green('Conectado com sucesso no Database!'));
+  }
 });
+
+export default db;
